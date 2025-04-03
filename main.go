ab72736f45f40738ebfd717f2751aa5f42ddef01
KousseilaMadi <kousseilamadi@yahoo.com>
2025-04-03 06:47:42 +0100

    init commit
    add_user added
    add_guest added
    add_product added

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"context"
+	"log" 
+	"github.com/jackc/pgx/v5"
+)
+
+var connUrl = "postgres://postgres:ukiyokaru@localhost:5432/mcdb"
+
+
+func root(w http.ResponseWriter, r *http.Request){
+  
+  fmt.Fprintf(w, "server works !");
+  body, err := io.ReadAll(r.Body)
+  if err != nil{
+    fmt.Println("error");
+  }
+  fmt.Println(string (body));
+}
+
+func add_user(w http.ResponseWriter, r *http.Request){
+  
+  body, err := io.ReadAll(r.Body)
+  if err != nil{
+    fmt.Println(err)
+  }
+
+  var jsonObj map[string]interface{};
+  json.Unmarshal(body, &jsonObj);
+
+
+  username := jsonObj["username"].(string)
+  Utype := jsonObj["type"].(string)
+  email := jsonObj["email"].(string)
+  password := jsonObj["password"].(string)
+  
+  conn, err := pgx.Connect(context.Background(), connUrl)
+  if err != nil {
+    log.Fatal("failed to connect to mcdb, err:", err)
+  }
+  
+  _, err1 := conn.Exec(context.Background(), "INSERT INTO public.\"Personne\"(username, type, email, password) VALUES('"+username+"', B'"+Utype+"', '"+email+"', '"+password+"')")
+  if err1 != nil{
+    fmt.Println("failed to insert, err:", err1);
+  }
+
+  
+}
+func add_guest(w http.ResponseWriter, r *http.Request){
+  body, err := io.ReadAll(r.Body)
+  if err != nil{
+    fmt.Println(err)
+  }
+
+  var jsonObj map[string]interface{};
+  json.Unmarshal(body, &jsonObj);
+
+  username := jsonObj["username"].(string)
+  Utype := jsonObj["type"].(string)
+  
+  conn, err := pgx.Connect(context.Background(), connUrl)
+  if err != nil {
+    log.Fatal("failed to connect to mcdb, err:", err)
+  }
+  
+  _, err1 := conn.Exec(context.Background(), "INSERT INTO public.\"Personne\"(username, type) VALUES('"+username+"', B'"+Utype+"')")
+  if err1 != nil{
+    fmt.Println("failed to insert, err:", err1);
+  }
+}
+
+
+func add_product(w http.ResponseWriter, r *http.Request){
+  body, err := io.ReadAll(r.Body)
+  if err != nil{
+    fmt.Println(err)
+  }
+
+  var jsonObj map[string]interface{};
+  json.Unmarshal(body, &jsonObj);
+
+  title := jsonObj["title"].(string)
+  description := jsonObj["description"].(string)
+  category := jsonObj["category"].(string)
+  source := jsonObj["source"].(string)
+  email := jsonObj["email"].(string)
+  price := jsonObj["price"].(string)
+  currency := jsonObj["currency"].(string)
+  phoneNumber := jsonObj["phoneNumber"].(string)
+  personUsername := jsonObj["personUsername"].(string)
+  
+  conn, err := pgx.Connect(context.Background(), connUrl)
+  if err != nil {
+    log.Fatal("failed to connect to mcdb, err:", err)
+  }
+  
+  var selectPersonId string = "(Select \"personId\" from public.\"Personne\" where username ='"+personUsername+"')"
+   
+  _, err1 := conn.Exec(context.Background(), "INSERT INTO public.\"Produit\"(title, description, category, source, email, price, currency, \"phoneNumber\", \"personId\", date) VALUES('"+title+"', '"+description+"', '"+category+"', B'"+source+"', '"+email+"', "+price+", '"+currency+"', '"+phoneNumber+"', "+selectPersonId+", NOW())")
+  if err1 != nil{
+    fmt.Println("failed to insert, err:", err1);
+  }
+}
+
+
+
+
+
+
+func main(){
+
+
+  http.HandleFunc("/add_user", add_user);
+  http.HandleFunc("/add_guest", add_guest);
+  http.HandleFunc("/add_product", add_product);
+  // http.HandleFunc("/add_comment", add_comment);
+  // http.HandleFunc("/report_product", report_product);
+  // http.HandleFunc("/buy_product", buy_product);
+  // http.HandleFunc("/login", login);
+  // http.HandleFunc("/fetch_products", fetch_products);
+  // http.HandleFunc("/fetch_user_products", fetch_products);
+  // http.HandleFunc("/fetch_users", fetch_users);
+  // http.HandleFunc("/fetch_comments", fetch_comments);
+  // http.HandleFunc("/fetch_reports", fetch_reports);
+  // http.HandleFunc("/fetch_user", root);
+  // http.HandleFunc("/update_user", root);
+  // http.HandleFunc("/", root);
+  // http.HandleFunc("/", root);
+  // http.HandleFunc("/", root);
+  http.HandleFunc("/", root);
+  fmt.Println("server listening on 8000")
+  http.ListenAndServe(":8000", nil);
+}
